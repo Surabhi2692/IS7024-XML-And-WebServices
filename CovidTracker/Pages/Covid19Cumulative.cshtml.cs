c236297e0e8d2f445cd9037aebac230c2cb48111
akunurla <akunurulaasya@gmail.com>
2021-11-09 22:16:46 -0500

    Added code to Covid19Cumulative.cshtml.cs

--- CovidTracker/Pages/Covid19Cumulative.cshtml.cs
+++ CovidTracker/Pages/Covid19Cumulative.cshtml.cs
@@ -11,6 +11,16 @@ namespace CovidTracker.Pages
     {
         public void OnGet()
         {
+            using (var webClient = new WebClient())
+            {
+                string jsonString = webClient.DownloadString("https://data.cdc.gov/resource/9mfq-cb36.json");
+                string jsonStringVaccineData = webClient.DownloadString("https://data.cdc.gov/resource/rh2h-3yt2.json");
+                var covid19CasesAndDeaths = CasesAndDeaths.Covid19CasesAndDeaths.FromJson(jsonString);
+                var covid19Vaccine = Vaccines.Covid19Vaccine.FromJson(jsonStringVaccineData);
+                ViewData["Covid19CasesAndDeaths"] = covid19CasesAndDeaths;
+                ViewData["Covid19Vaccines"] = covid19Vaccine;
+
+            }
         }
     }
 }
