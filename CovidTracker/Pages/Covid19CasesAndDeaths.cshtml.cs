d5f657ee8c256d97e2280435b0c0f530e15866b4
Surabhi Kulkarni <surabhikulkarni2692@gmail.com>
2021-11-11 23:58:16 -0500

    CovidTracker - Cases and Deaths UI

--- CovidTracker/Pages/Covid19CasesAndDeaths.cshtml.cs
+++ CovidTracker/Pages/Covid19CasesAndDeaths.cshtml.cs
@@ -5,19 +5,86 @@ using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CovidTracker.Pages
 {
     public class Covid19CasesAndDeathsModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public SelectList StateList { get; set; }
+        public string SearchState { get; set; }
+        public Dictionary<string, string> statesDictionary { get; set; }
+
+        public void OnGet(string q)
         {
+            InitStateDropdown();
             using (var webClient = new WebClient()) 
             {
                 string jsonString = webClient.DownloadString("https://data.cdc.gov/resource/9mfq-cb36.json");
                 var covid19CasesAndDeaths = CasesAndDeaths.Covid19CasesAndDeaths.FromJson(jsonString);
-                ViewData["Covid19CasesAndDeaths"] = covid19CasesAndDeaths;;
+                ViewData["Covid19CasesAndDeaths"] = covid19CasesAndDeaths[0];
             }
+        }
+
+        private void InitStateDropdown() 
+        {
+            statesDictionary = new Dictionary<string, string>
+            {
+                { "AL", "Alabama" },
+                { "AK", "Alaska" },
+                { "AZ", "Arizona" },
+                { "AR", "Arkansas" },
+                { "CA", "California" },
+                { "CO", "Colorado" },
+                { "CT", "Connecticut" },
+                { "DE", "Delaware" },
+                { "DC", "District Of Columbia" },
+                { "FL", "Florida" },
+                { "GA", "Georgia" },
+                { "HI", "Hawaii" },
+                { "ID", "Idaho" },
+                { "IL", "Illinois" },
+                { "IN", "Indiana" },
+                { "IA", "Iowa" },
+                { "KS", "Kansas" },
+                { "KY", "Kentucky" },
+                { "LA", "Louisiana" },
+                { "ME", "Maine" },
+                { "MD", "Maryland" },
+                { "MA", "Massachusetts" },
+                { "MI", "Michigan" },
+                { "MN", "Minnesota" },
+                { "MS", "Mississippi" },
+                { "MO", "Missouri" },
+                { "MT", "Montana" },
+                { "NE", "Nebraska" },
+                { "NV", "Nevada" },
+                { "NH", "New Hampshire" },
+                { "NJ", "New Jersey" },
+                { "NM", "New Mexico" },
+                { "NY", "New York" },
+                { "NC", "North Carolina" },
+                { "ND", "North Dakota" },
+                { "OH", "Ohio" },
+                { "OK", "Oklahoma" },
+                { "OR", "Oregon" },
+                { "PA", "Pennsylvania" },
+                { "RI", "Rhode Island" },
+                { "SC", "South Carolina" },
+                { "SD", "South Dakota" },
+                { "TN", "Tennessee" },
+                { "TX", "Texas" },
+                { "UT", "Utah" },
+                { "VT", "Vermont" },
+                { "VA", "Virginia" },
+                { "WA", "Washington" },
+                { "WV", "West Virginia" },
+                { "WI", "Wisconsin" },
+                { "WY", "Wyoming" }
+            };
+
+            //ViewData["SearchState"] = new SelectList(statesDictionary, "State", "Name");
         }
     }
 }
