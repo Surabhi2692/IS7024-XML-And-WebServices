1038ebcd824f097ae70ea64ed57f21df432d5cf1
Daya <69583015+DN8630@users.noreply.github.com>
2021-11-12 12:41:33 -0500

    Backend and UI integration fixes

--- CovidTracker/Pages/Covid19Vaccines.cshtml.cs
+++ CovidTracker/Pages/Covid19Vaccines.cshtml.cs
@@ -29,13 +29,20 @@ namespace CovidTracker.Pages
                 if (!string.IsNullOrWhiteSpace(query))
                 {
                     var covid19VaccineList = covid19Vaccines.ToList();
-                    var stateWiseVaccines = covid19VaccineList.FindAll(x => string.Equals(x.Location, query, StringComparison.OrdinalIgnoreCase)).Where(x => x.DateType.ToString() == "Report");
-                    if (stateWiseVaccines != null)
+                    var stateWiseVaccines = covid19VaccineList.FindAll(x => string.Equals(x.Location, query, StringComparison.OrdinalIgnoreCase)).Where(x => x.DateType.ToString() == "Report").ToList();
+                    if (stateWiseVaccines != null && stateWiseVaccines.Count > 0)
                     {
                         var orderedStateWiseVaccines = stateWiseVaccines.OrderByDescending(x => x.Date).ToArray();
                         ViewData["Covid19Vaccines"] = orderedStateWiseVaccines[0];
                     }
-
+                    else
+                    {
+                        ViewData["Covid19CasesAndDeaths"] = null;
+                    }
+                }
+                else
+                {
+                    ViewData["Covid19Vaccines"] = null;
                 }
             }
         }
@@ -98,65 +105,8 @@ namespace CovidTracker.Pages
             };
         }
 
-        private void InitStateDropdown()
-        {
-            statesDictionary = new Dictionary<string, string>
-            {
-                { "AL", "Alabama" },
-                { "AK", "Alaska" },
-                { "AZ", "Arizona" },
-                { "AR", "Arkansas" },
-                { "CA", "California" },
-                { "CO", "Colorado" },
-                { "CT", "Connecticut" },
-                { "DE", "Delaware" },
-                { "DC", "District Of Columbia" },
-                { "FL", "Florida" },
-                { "GA", "Georgia" },
-                { "HI", "Hawaii" },
-                { "ID", "Idaho" },
-                { "IL", "Illinois" },
-                { "IN", "Indiana" },
-                { "IA", "Iowa" },
-                { "KS", "Kansas" },
-                { "KY", "Kentucky" },
-                { "LA", "Louisiana" },
-                { "ME", "Maine" },
-                { "MD", "Maryland" },
-                { "MA", "Massachusetts" },
-                { "MI", "Michigan" },
-                { "MN", "Minnesota" },
-                { "MS", "Mississippi" },
-                { "MO", "Missouri" },
-                { "MT", "Montana" },
-                { "NE", "Nebraska" },
-                { "NV", "Nevada" },
-                { "NH", "New Hampshire" },
-                { "NJ", "New Jersey" },
-                { "NM", "New Mexico" },
-                { "NY", "New York" },
-                { "NC", "North Carolina" },
-                { "ND", "North Dakota" },
-                { "OH", "Ohio" },
-                { "OK", "Oklahoma" },
-                { "OR", "Oregon" },
-                { "PA", "Pennsylvania" },
-                { "RI", "Rhode Island" },
-                { "SC", "South Carolina" },
-                { "SD", "South Dakota" },
-                { "TN", "Tennessee" },
-                { "TX", "Texas" },
-                { "UT", "Utah" },
-                { "VT", "Vermont" },
-                { "VA", "Virginia" },
-                { "WA", "Washington" },
-                { "WV", "West Virginia" },
-                { "WI", "Wisconsin" },
-                { "WY", "Wyoming" }
-            };
-
-            //ViewData["SearchState"] = new SelectList(statesDictionary, "State", "Name");
+       
         }
 
     }
-}
+
