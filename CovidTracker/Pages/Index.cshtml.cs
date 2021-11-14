ea8875268e6f5ecd6a0528723775f85058f9375b
eShravankumar9 <89714288+eShravankumar9@users.noreply.github.com>
2021-11-14 01:06:34 -0500

    Json schema validation

--- CovidTracker/Pages/Index.cshtml.cs
+++ CovidTracker/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Schema;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +24,22 @@ namespace CovidTracker.Pages
             using (var webClient = new WebClient())
             {
                 string jsonString = webClient.DownloadString("https://data.cdc.gov/resource/9mfq-cb36.json");
+
+                JSchema schema = JSchema.Parse(System.IO.File.ReadAllText("StockSchema.json"));
+                IList<string> validationEvents = new List<string>();
+                JObject jsonObject = JObject.Parse(jsonString);
+                TrendingStock result = new TrendingStock();
+                if (jsonObject.IsValid(schema, out validationEvents))
+                {
+                    result = TrendingStock.FromJson(jsonString);
+                }
+                else
+                {
+                    //error handling
+                }
+
+
+
                 string jsonStringVaccineData = webClient.DownloadString("https://data.cdc.gov/resource/rh2h-3yt2.json");
                 var covid19CasesAndDeaths = CasesAndDeaths.Covid19CasesAndDeaths.FromJson(jsonString);
                 var covid19Vaccine = Vaccines.Covid19Vaccine.FromJson(jsonStringVaccineData);
